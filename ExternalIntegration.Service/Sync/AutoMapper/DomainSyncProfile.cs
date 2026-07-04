441d0f15b059f9bde217ed93979a7dc0ec8a700a
Neda Nasirizadeh <n.nasirizadeh@N-NASIRIZADEH>
2026-07-04 09:43:02 +0330

    change domain map

--- ExternalIntegration.Service/Sync/AutoMapper/DomainSyncProfile.cs
+++ ExternalIntegration.Service/Sync/AutoMapper/DomainSyncProfile.cs
@@ -9,11 +9,37 @@ namespace ExternalIntegration.Service.Sync.AutoMapper
     {
         public DomainSyncProfile()
         {
-            CreateMap<GoodwayBillDto, GoodwayBill>()
-           .ForMember(dest => dest.BulkList, opt => opt.MapFrom(src => JsonConvert.SerializeObject(src.BulkList)))
-           .ForMember(dest => dest.CargoList, opt => opt.MapFrom(src => JsonConvert.SerializeObject(src.CargoList)))
-           .ForMember(dest => dest.ContainerList, opt => opt.MapFrom(src => JsonConvert.SerializeObject(src.ContainerList)));
+            CreateMap<GoodwayBill, GoodwayBillDto>()
+               .ForMember(dest => dest.BulkList, opt => opt.MapFrom(src => JsonConvert.DeserializeObject<List<BulkResultDto>>(src.BulkList)))
+               .ForMember(dest => dest.CargoList, opt => opt.MapFrom(src => JsonConvert.DeserializeObject<List<GeneralCargoResultDto>>(src.CargoList)))
+               .ForMember(dest => dest.ContainerList, opt => opt.MapFrom(src => JsonConvert.DeserializeObject<List<ContainerResultDto>>(src.ContainerList)));
+
             CreateMap<DischargePermitDto, DischargePermit>();
+
+
+            //CreateMap<GoodwayBill, GoodwayBillDto>()
+            //        .ForMember(dest => dest.BulkList,
+            //            opt => opt.MapFrom(src => ParseJsonSafe<List<BulkDto>>(src.BulkList)))
+            //        .ForMember(dest => dest.CargoList,
+            //            opt => opt.MapFrom(src => ParseJsonSafe<List<GeneralCargoDto>>(src.CargoList)));
+
+
+        }
+        private T ParseJsonSafe<T>(string json) where T : new()
+        {
+            if (string.IsNullOrWhiteSpace(json) || json == "[]")
+                return new T();
+
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(json) ?? new T();
+            }
+            catch (Exception ex)
+            {
+                // لاگ برای دیباگ
+                // Debug.WriteLine($"JSON Parse Error: {ex.Message}");
+                return new T();
+            }
         }
     }
 }
