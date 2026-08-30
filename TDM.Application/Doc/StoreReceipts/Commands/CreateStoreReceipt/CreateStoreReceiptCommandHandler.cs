a1fb42a4e61facdc24939cf76893fb2af70e1490
nasiri <nasiri pc@DESKTOP-R2JF3CO>
2026-08-30 14:25:58 +0330

    add handller

--- TDM.Application/Doc/StoreReceipts/Commands/CreateStoreReceipt/CreateStoreReceiptCommandHandler.cs
+++ TDM.Application/Doc/StoreReceipts/Commands/CreateStoreReceipt/CreateStoreReceiptCommandHandler.cs
@@ -1,10 +1,54 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TDM.Application.Common.Interfaces;
+using TDM.Application.Doc.StoreReceipt.Command.CreateStoreReceipt;
+using TDM.Domain.Entities;
 
 namespace TDM.Application.Doc.StoreReceipts.Commands.CreateStoreReceipt
 {
-    internal class CreateStoreReceiptCommandHandler
-    {
-    }
+   
+        public class CreateStoreReceiptCommandHandler : IRequestHandler<CreateStoreReceiptCommand, Guid>
+        {
+            private readonly IRepository<StoreReceiptHead> _storeReceiptHeadRepository;
+            private readonly IUnitOfWork _unitOfWork;
+
+            public CreateStoreReceiptCommandHandler(IUnitOfWork unitOfWork
+                , IRepository<StoreReceiptHead> storeReceiptHeadRepository)
+            {
+                _unitOfWork = unitOfWork;
+                _storeReceiptHeadRepository = storeReceiptHeadRepository;
+            }
+
+            public async Task<Guid> Handle(CreateStoreReceiptCommand request, CancellationToken cancellationToken)
+            {
+
+                var storeReceiptHead = new StoreReceiptHead(
+                                            request.TerminalCode,
+                                            request.IPASStoreReceiptNo,
+                                            request.IssueDate,
+                                            request.ConsigneeId,
+                                            request.ConsigneeRepId,
+                                            request.CargoTypeId,
+                                            request.FirstDischargeDate,
+                                            request.CreatorId,
+                                            request.TrafficId,
+                                            request.StoreReceiptStateId,
+                                            request.RequestId,
+                                            request.VoyageNoticeNo,
+                                            request.ArrivalTypeId,
+                                            request.DeclarationId,
+                                            request.BillOfLadingId
+                                        );
+
+
+                await _storeReceiptHeadRepository.InsertAsync(storeReceiptHead);
+
+                await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+                return storeReceiptHead.Id;
+            }
+        }
+    
 }
