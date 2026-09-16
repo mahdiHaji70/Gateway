685a914ed0984f39efc61942808c2c2dee6aaf8a
Neda Nasirizadeh <n.nasirizadeh@N-NASIRIZADEH>
2026-09-16 11:48:36 +0330

    send array store to ipas

--- TDM.Application/Doc/StoreReceipts/Commands/SendIpasStoreAllocation/SendIpasStoreAllocationRequestMapper.cs
+++ TDM.Application/Doc/StoreReceipts/Commands/SendIpasStoreAllocation/SendIpasStoreAllocationRequestMapper.cs
@@ -1,5 +1,6 @@
 using TDM.Domain.Entities;
 using TDM.Domain.Enums;
+using System.Collections.Generic;
 namespace TDM.Application.Doc.StoreReceipts.Commands.SendIpasStoreAllocation
 {
     public static class SendIpasStoreAllocationRequestMapper
@@ -47,6 +48,49 @@ namespace TDM.Application.Doc.StoreReceipts.Commands.SendIpasStoreAllocation
                     Quantity = container.StoreReceiptContainerGoods.Count
                 });
             }
+            return request;
+        }
+
+        public static SendIpasStoreAllocationRequest Map(
+            StoreReceiptHead receipt,
+            IEnumerable<TerminalDischarge> terminalDischarges)
+        {
+            var request = new SendIpasStoreAllocationRequest
+            {
+                StoreReceiptId = receipt.Id,
+                TerminalCode = receipt.TerminalCode
+            };
+
+            var groupedDischarges = terminalDischarges
+                .GroupBy(x => x.Store.Code, StringComparer.OrdinalIgnoreCase);
+
+            foreach (var storeGroup in groupedDischarges)
+            {
+                var firstDischarge = storeGroup.First();
+                var commodity = firstDischarge.DeclarationItem.Commodity;
+
+                request.Goods.Add(new SendIpasStoreAllocationGoodRequest
+                {
+                    CargoType = receipt.CargoTypeId == CargoTypes.Bulk
+                        ? "Bulk"
+                        : "GeneralCargo",
+                    OperationDate = storeGroup.Max(x => x.DischargeDate),
+                    StorageAreaCode = storeGroup.Key,
+                    HsCode = commodity.HsCode,
+                    Description = commodity.Name,
+                    PackageTypeCode = firstDischarge.DeclarationItem.Package.Code,
+                    PackageQuantity = storeGroup.Sum(x => x.PackNB),
+                    GrossWeight = storeGroup.Sum(x => x.Weight),
+                    NetWeight = storeGroup.Sum(x => x.Weight),
+                    Volume = storeGroup.Sum(x => x.Volume),
+                    IsNonPalletized = storeGroup.Any(x => x.IsNonPalletized),
+                    IsDamaged = storeGroup.Any(x => x.IsDamaged),
+                    IsDangerous = storeGroup.Any(x => x.IsDangerous),
+                    IsVoluminous = storeGroup.Any(x => x.IsVoluminous),
+                    IsHeavy = null
+                });
+            }
+
             return request;
         }
     }
